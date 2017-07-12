ff3ad6b0d48efef75430cb9201970da12c388541
Yas <siyahdeniz@gmail.com>
2017-07-12 18:46:14 +0300

    PythohDude uses installed python.exe. Editor is ICSharp Editor.

---
+++ PythonDude/frmMain.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace PythonDude
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbOpenFile = new System.Windows.Forms.ToolStripButton();
+            this.tsbSave = new System.Windows.Forms.ToolStripButton();
+            this.tsbSaveAs = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbRun = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblFileName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.rtbCode = new ICSharpCode.TextEditor.TextEditorControl();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.defToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ifToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbOpenFile,
+            this.tsbSave,
+            this.tsbSaveAs,
+            this.toolStripSeparator1,
+            this.tsbRun,
+            this.toolStripSeparator2,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(949, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbOpenFile
+            // 
+            this.tsbOpenFile.Image = ((System.Drawing.Image)(resources.GetObject("tsbOpenFile.Image")));
+            this.tsbOpenFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbOpenFile.Name = "tsbOpenFile";
+            this.tsbOpenFile.Size = new System.Drawing.Size(76, 22);
+            this.tsbOpenFile.Text = "Dosya Aç";
+            this.tsbOpenFile.Click += new System.EventHandler(this.tsbOpenFile_Click);
+            // 
+            // tsbSave
+            // 
+            this.tsbSave.Image = ((System.Drawing.Image)(resources.GetObject("tsbSave.Image")));
+            this.tsbSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSave.Name = "tsbSave";
+            this.tsbSave.Size = new System.Drawing.Size(63, 22);
+            this.tsbSave.Text = "Kaydet";
+            this.tsbSave.Click += new System.EventHandler(this.tsbSave_Click);
+            // 
+            // tsbSaveAs
+            // 
+            this.tsbSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("tsbSaveAs.Image")));
+            this.tsbSaveAs.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSaveAs.Name = "tsbSaveAs";
+            this.tsbSaveAs.Size = new System.Drawing.Size(94, 22);
+            this.tsbSaveAs.Text = "Farklı Kaydet";
+            this.tsbSaveAs.Click += new System.EventHandler(this.tsbSaveAs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbRun
+            // 
+            this.tsbRun.Image = ((System.Drawing.Image)(resources.GetObject("tsbRun.Image")));
+            this.tsbRun.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRun.Name = "tsbRun";
+            this.tsbRun.Size = new System.Drawing.Size(63, 22);
+            this.tsbRun.Text = "Çalıştır";
+            this.tsbRun.Click += new System.EventHandler(this.tsbRun_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblFileName});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 591);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(949, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(16, 17);
+            this.lblFileName.Text = "...";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.rtbCode);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtbConsole);
+            this.splitContainer1.Size = new System.Drawing.Size(949, 566);
+            this.splitContainer1.SplitterDistance = 463;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbConsole.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtbConsole.Location = new System.Drawing.Point(0, 0);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.Size = new System.Drawing.Size(482, 566);
+            this.rtbConsole.TabIndex = 1;
+            this.rtbConsole.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // rtbCode
+            // 
+            this.rtbCode.AutoScroll = true;
+            this.rtbCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbCode.IsReadOnly = false;
+            this.rtbCode.LineViewerStyle = ICSharpCode.TextEditor.Document.LineViewerStyle.FullRow;
+            this.rtbCode.Location = new System.Drawing.Point(0, 0);
+            this.rtbCode.Name = "rtbCode";
+            this.rtbCode.Size = new System.Drawing.Size(463, 566);
+            this.rtbCode.TabIndex = 1;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.ifToolStripMenuItem,
+            this.forToolStripMenuItem,
+            this.defToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(41, 22);
+            this.toolStripDropDownButton1.Text = "Kod";
+            // 
+            // defToolStripMenuItem
+            // 
+            this.defToolStripMenuItem.Name = "defToolStripMenuItem";
+            this.defToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.defToolStripMenuItem.Text = "def";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // ifToolStripMenuItem
+            // 
+            this.ifToolStripMenuItem.Name = "ifToolStripMenuItem";
+            this.ifToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ifToolStripMenuItem.Text = "if";
+            // 
+            // forToolStripMenuItem
+            // 
+            this.forToolStripMenuItem.Name = "forToolStripMenuItem";
+            this.forToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.forToolStripMenuItem.Text = "for";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 613);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Python Dude";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbOpenFile;
+        private System.Windows.Forms.ToolStripButton tsbSave;
+        private System.Windows.Forms.ToolStripButton tsbSaveAs;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbRun;
+        private System.Windows.Forms.RichTextBox rtbConsole;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel lblFileName;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private ICSharpCode.TextEditor.TextEditorControl rtbCode;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ifToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem defToolStripMenuItem;
+    }
+}
+
---
+++ PythonDude/frmMain.cs
@@ -0,0 +1,107 @@
+﻿using ICSharpCode.TextEditor.Document;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PythonDude
+{
+    public partial class frmMain : Form
+    {
+        private void AddCode(string code)
+        {
+            rtbCode.ActiveTextAreaControl.TextArea.InsertString(code);
+        }
+
+        private void RunPython(string args)
+        {
+            string s = DOSPrompt.CMDAutomate(args, "C:\\python3.5\\python.exe");
+            rtbConsole.Text = s;
+        }
+
+        private void LoadFile()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rtbCode.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                lblFileName.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void SaveFile()
+        {
+            System.IO.File.WriteAllText(lblFileName.Text, rtbCode.Text);
+            // rtbCode.SaveFile(lblFileName.Text, RichTextBoxStreamType.PlainText);
+
+        }
+
+        private void SaveFileAs()
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, rtbCode.Text);
+                lblFileName.Text = saveFileDialog1.FileName;
+            }
+
+        }
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tsbRun_Click(object sender, EventArgs e)
+        {
+            RunPython("-c \"" + rtbCode.Text + "\"");
+        }
+
+        private void tsbOpenFile_Click(object sender, EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void tsbSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void tsbSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileAs();
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+
+            FileSyntaxModeProvider fsmp = new FileSyntaxModeProvider(Application.StartupPath);
+
+            HighlightingManager.Manager.AddSyntaxModeFileProvider(fsmp);
+            rtbCode.SetHighlighting("Python");
+
+
+
+        }
+
+        private void rtbCode_TextChanged(object sender, EventArgs e)
+        {
+            //          
+        }
+
+        #region ADDCODE
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddCode("print()");
+        }
+
+        #endregion 
+    }
+}
